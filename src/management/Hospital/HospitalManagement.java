05da0f7d1e4b69b5f5fe7d7f8afc9300ba84ffd8
keerthana <keerthanakumaravel2003@gmail.com>
2025-06-02 00:08:25 +0530

    HospitalManagement validations were added to addPatient

--- src/management/Hospital/HospitalManagement.java
+++ src/management/Hospital/HospitalManagement.java
@@ -1,9 +1,6 @@
 package management.Hospital;
 
-import com.sun.org.apache.bcel.internal.generic.DCONST;
 
-import javax.jws.Oneway;
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,22 +10,45 @@ public class HospitalManagement {
     static List<Doctor> doctors=new ArrayList<>();
     List <Patient> patients = new ArrayList<>();
     public void init(){
-        doctors.add(new Doctor("name2", "B"));
-        doctors.add(new Doctor("name 3","C"));
         Doctor doctor1=new Doctor("name1", "A");
         doctors.add(doctor1);
+        doctors.add(new Doctor("name2", "B"));
+        doctors.add(new Doctor("name 3","C"));
+
 
     }
     public void addPatient(Patient patient){
-        System.out.println("patient = " + patient);
+//        System.out.println("patient = " + patient);
 
         String name = patient.getName();
         String role = patient.getRole();
-
-        for (Doctor doc : doctors){
+        Integer doctorIndex = null;
+        if(doctors.size()==0 || wardSize==0){
+            System.out.println("Doctors/Ward are not available");
+            return;
+        }
+        for (int i = 0; i<doctors.size(); i++){
+            Doctor doc=doctors.get(i);
             if (doc.getRole().equals(role)){
-                // wards
+                if(wardSize!=0){
+                    Ward wardObj = new Ward(1, doc, patient );
+                    wardSize--;
+                    doctorIndex=i;
+                    break;
+//                    doctors.remove(doc);
+                }
+            }else{
+                System.out.println("Unmatched Doctor role: " +doc.getRole());
             }
+        }
+//        System.out.println("doctorIndex = " + doctorIndex);
+        if(doctorIndex!=null){
+
+            Doctor isremove =  doctors.remove(doctorIndex.intValue());
+//            System.out.println("isremove = " + isremove);
+        }
+        else{
+            System.out.println("The doctor is busy: " +patient.getRole());
         }
 
 
@@ -40,5 +60,10 @@ public class HospitalManagement {
         System.out.println(doctors);
 
         manager.addPatient(new Patient("Barath" , "A" ,"xyz"));
+        manager.addPatient(new Patient("Keerthana" , "D" ,"xz"));
+//        manager.addPatient(new Patient("Keerths" , "C" ,"xz"));
+//        manager.addPatient(new Patient("Keerthana" , "A" ,"xz"));
+        System.out.println("doctors.size() = " + doctors.size());
+        System.out.println("wardSize = " + wardSize);
     }
 }
