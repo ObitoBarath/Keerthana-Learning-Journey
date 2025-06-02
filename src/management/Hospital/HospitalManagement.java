382dfdfe8fe08fd2c00a9bb3ad5f87404cb82b97
keerthana <keerthanakumaravel2003@gmail.com>
2025-06-02 22:15:27 +0530

    patientDataFromUser method is added.

--- src/management/Hospital/HospitalManagement.java
+++ src/management/Hospital/HospitalManagement.java
@@ -3,6 +3,7 @@ package management.Hospital;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class HospitalManagement {
     static Integer doctorCount = 3;
@@ -54,16 +55,57 @@ public class HospitalManagement {
 
 
     }
+    public void patientDataFromUser(Scanner sc){
+        System.out.println("Incoming Patients");
+//        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter patient name: ");
+        String nam = sc.nextLine();
+        for (Doctor docRoles :doctors){
+            String roles = docRoles.getRole();
+            System.out.println("Available doctors :" +roles);
+        }
+//        System.out.println("available doctors :" + .getRole());
+        System.out.println("Enter the doctor's role: ");
+        String rol = sc.nextLine();
+        System.out.println("Enter the disease: ");
+        String dis = sc.nextLine();
+        addPatient(new Patient(nam, rol, dis));
+//        Patient pat = new Patient()
+
+    }
     public static void main(String[] args){
         HospitalManagement  manager= new HospitalManagement();
         manager.init();
         System.out.println(doctors);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+//            manager.patientDataFromUser(sc);
+            System.out.println("1) add patient: ");
+            System.out.println("2) getting doctor details: ");
+            System.out.println("3) get patient list: ");
+            System.out.println("4) exit: ");
+            System.out.println("Enter a number: ");
+            int number = sc.nextInt();
 
-        manager.addPatient(new Patient("Barath" , "A" ,"xyz"));
-        manager.addPatient(new Patient("Keerthana" , "D" ,"xz"));
+            switch(number) {
+                case 1 -> {
+                    manager.patientDataFromUser(sc);
+                }
+                case 4 -> {
+                    return;
+                }
+                default -> {
+                    return;
+                }
+            }
+
+        }
+
+//        manager.addPatient(new Patient("Barath" , "A" ,"xyz"));
+//        manager.addPatient(new Patient("Keerthana" , "D" ,"xz"));
 //        manager.addPatient(new Patient("Keerths" , "C" ,"xz"));
 //        manager.addPatient(new Patient("Keerthana" , "A" ,"xz"));
-        System.out.println("doctors.size() = " + doctors.size());
-        System.out.println("wardSize = " + wardSize);
+//        System.out.println("doctors.size() = " + doctors.size());
+//        System.out.println("wardSize = " + wardSize);
     }
 }
