8a0d4e3f15e2a2b5dc978acff5820098c158aa5e
Barath <barath.b@centrico.tech>
2025-08-25 15:56:57 +0530

    Complete these exercise

---
+++ src/playground/PlayGround.java
@@ -0,0 +1,70 @@
+package playground;
+
+public class PlayGround {
+
+//1. Count words starting with "A"
+//List<String> words = Arrays.asList("Apple", "Banana", "Avocado", "Mango", "Apricot");
+
+// 2. Sort a list of strings by length
+//    List<String> words = Arrays.asList("Java", "Spring", "Hibernate", "React");
+
+
+//3. Group employees by department
+
+/*
+
+class Employee {
+    String name;
+    String department;
+
+    Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
+  List<Employee> employees = Arrays.asList(
+            new Employee("Alice", "IT"),
+            new Employee("Bob", "HR"),
+            new Employee("Charlie", "IT"),
+            new Employee("David", "Finance"),
+            new Employee("Eva", "HR")
+        );
+ */
+
+// 4.          Math
+
+/*
+List<List<Integer>> lists = List.of(List.of(1, 2, 3), List.of(4, 5), List.of(6, 7, 8, 9));
+     Multiply each list value by 2 and add them
+
+     (1 *2) + (2*2) + (3*2) ,  (4*2) + (5*2) ,  (6*2) + (7*2) + (8*2) + (9*2)
+     add them up and get the first highest value and multiply with the second highest value and divide by 4.5 and print the result with 5 digits after decimal point
+    (12 , 18 , 30) => 30 * 18 / 4.5 = 120.00000 // final answer
+    */
+
+
+    //5. Create a Custom Map named as  PalazzoMap and it should have below functionalities
+
+    // minimum capacity of map should be 16
+    /*
+        1. put(key, value) -> to add key value pair
+        2. get(key) -> to get value by key
+        3. remove(key) -> to remove key value pair by key
+        4. containsKey(key) -> to check if key exists
+        5. size() -> to get the size of the map
+        6. isEmpty() -> to check if the map is empty
+        7. clear() -> to clear the map
+        8. keySet() -> to get all keys in the map
+        9. values() -> to get all values in the map
+        10. entrySet() -> to get all key value pairs in the map
+     */
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+}
